04872d78a9e0efa856fca3ec18781a6119819a92
mengen.dai <mengen.dai@outlook.com>
2024-11-26 22:35:49 +0800

    feat(schedule): 添加高亮隐藏按钮功能
    - 在 table-header-title组件中添加高亮效果
    - 实现隐藏按钮的高亮逻辑
    - 更新样式文件，增加 .table-header-title 类样式

--- src/components/schedule/schedule-maintenance/segments/table-header-title/table-header-title.tsx
+++ src/components/schedule/schedule-maintenance/segments/table-header-title/table-header-title.tsx
@@ -1,5 +1,5 @@
 import "./table-header-title.scss";
-import React from "react";
+import React, {useState} from "react";
 import {Button, Flex, Popover} from "antd";
 import {HideIcon01} from "@D/icons/hide-icon-01";
 import {ProColumns} from "@ant-design/pro-table";
@@ -13,17 +13,24 @@ export const TableHeaderTitle: React.FC<{
     showColumns: (React.Key | undefined)[];
     setShowColumns: React.Dispatch<React.SetStateAction<(React.Key | undefined)[]>>;
 }> = ({columns, showColumns, setShowColumns}) => {
+    const [hideBtnId, setHideBtnId] = useState<string | undefined>(undefined);
+
     return (
         <div className={"table-header-title"}>
             <Flex gap={20}>
                 <Popover title={"Display columns"}
                          trigger="click"
                          arrow={false}
+                         onOpenChange={open => {
+                             setHideBtnId(open ? "highlight-button" : undefined);
+                         }}
                          placement="bottomLeft"
                          content={<TableColumnDisplay columns={columns}
                                                       showColumns={showColumns}
                                                       setShowColumns={setShowColumns}/>}>
-                    <Button type="text" icon={<HideIcon01 width={16} height={16} color={"#000000"}/>}>
+                    <Button type="text"
+                            id={hideBtnId}
+                            icon={<HideIcon01 width={16} height={16} color={"#000000"}/>}>
                         Hide
                     </Button>
                 </Popover>
