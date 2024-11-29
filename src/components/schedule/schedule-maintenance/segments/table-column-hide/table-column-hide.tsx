8322246c2493a056cc8e66984022ea9063f10780
mengen.dai <mengen.dai@outlook.com>
2024-11-29 22:54:07 +0800

    feat(schedule): 优化时间表维护页面的列隐藏功能
    - 添加搜索功能，用户可以通过输入框查找需要显示/隐藏的列

--- src/components/schedule/schedule-maintenance/segments/table-column-hide/table-column-hide.tsx
+++ src/components/schedule/schedule-maintenance/segments/table-column-hide/table-column-hide.tsx
@@ -1,41 +1,58 @@
 import React, {useState} from "react";
-import {Button, Checkbox, Flex, Popover} from "antd";
+import {Button, Checkbox, Flex, Input, Popover, Space} from "antd";
 import {ProColumns} from "@ant-design/pro-table";
 import {MaintainScheduleTableRow} from "@D/components/schedule/schedule-maintenance/schedule-maintenance-types";
 import {HideIcon01} from "@D/icons/hide-icon-01";
 import {HIGHLIGHT_COLOR} from "@D/core/style/theme";
+import {SearchIcon01} from "@D/icons/search-icon-01";
 
 export const TableColumnHide: React.FC<{
     columns: Array<ProColumns<MaintainScheduleTableRow>>;
     showColumns: (React.Key | undefined)[];
     setShowColumns: React.Dispatch<React.SetStateAction<(React.Key | undefined)[]>>;
 }> = ({columns, showColumns, setShowColumns}) => {
+    const [options, setOptions] = useState(columns);
     const [highlight, setHighlight] = useState<boolean>();
     const checkAll = columns.length === showColumns.length;
     const indeterminate = showColumns.length > 0 && showColumns.length < columns.length;
     return (
-        <Popover title={"Display columns"}
+        <Popover title={<span style={{fontWeight: "bold", fontSize: "18px"}}>Display columns</span>}
                  arrow={false}
                  trigger={"click"}
                  placement={"bottomLeft"}
-                 onOpenChange={setHighlight}
-                 content={<>
-                     <Checkbox indeterminate={indeterminate} onChange={(e) => {
+                 destroyTooltipOnHide={true}
+                 onOpenChange={open => {
+                     setHighlight(open);
+                     open && setOptions(columns);
+                 }}
+                 content={<Space direction={"vertical"} size={"small"} style={{width: 300}}>
+                     <Input placeholder="Find columns to show/hide"
+                            onChange={e => {
+                                const searchValue = e.target.value;
+                                const regex = new RegExp(searchValue, "i");
+                                setOptions(columns.filter(column => regex.test(column.title as string)));
+                            }}
+                            suffix={<SearchIcon01 width={16} height={16} color={"#bfbfbf"}/>}/>
+                     <Checkbox indeterminate={indeterminate} onChange={e => {
                          setShowColumns(e.target.checked ? columns.map(column => column.key) : []);
                      }} checked={checkAll}>
-                         Check all
+                         <span style={{fontWeight: "bold"}}>All columns</span>
+                         <span>&nbsp;&nbsp;&nbsp;</span>
+                         <span style={{color: "#7e7f8d"}}>{showColumns.length} selected</span>
                      </Checkbox>
-                     <Checkbox.Group style={{width: "100%"}}
-                                     value={showColumns}
-                                     onChange={(checkedValues) => setShowColumns(checkedValues)}>
-                         <Flex vertical={true} gap={"small"}>
-                             {
-                                 columns.map(column =>
-                                     <Checkbox key={column.key}
-                                               value={column.key}>{column.title as string}</Checkbox>)
-                             }
-                         </Flex>
-                     </Checkbox.Group></>}>
+                     <Flex vertical={true} gap={"small"}>
+                         {
+                             options.map(column =>
+                                 <Checkbox key={column.key}
+                                           value={column.key}
+                                           checked={showColumns.includes(column.key)}
+                                           onChange={e => {
+                                               setShowColumns(e.target.checked ? [...showColumns, column.key] : showColumns.filter(key => key !== column.key));
+                                           }}
+                                 >{column.title as string}</Checkbox>)
+                         }
+                     </Flex>
+                 </Space>}>
             <Button type="text"
                     style={{backgroundColor: highlight ? HIGHLIGHT_COLOR : ""}}
                     icon={<HideIcon01 width={16} height={16} color={"#000000"}/>}>
